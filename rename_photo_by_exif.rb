8d7b6cdc5720e681fa9cd9684bed69ed0f87202a
epik <epik@server.fake>
2014-01-15 12:10:40 +0100

    Work but problem is when two or more photos are taken in same time (second). This is TODO.

--- rename_photo_by_exif.rb
+++ rename_photo_by_exif.rb
@@ -4,18 +4,18 @@ require 'exifparser'
 class RenamePhotoByExif
   case
     when ARGV.size == 0 then
-      puts 'Podaj ścieżkę do zdjęć.'
+      puts 'Please, write path to photos as argument.'
     when ARGV.size == 1 then
       path = ARGV[0]
-
-      puts 'Zdjęcia w katalogu:'
       Dir.glob("#{path}/*.jpg") do |photo|
-        puts Pathname.new(photo).basename
+        exif = Exif::Parser.new(photo)
+        new_name = exif['DateTimeOriginal'].to_s.gsub(':', '_').sub(' ', '__')
+        File.rename(photo, path + '/' + new_name + '.jpg')
       end
     when ARGV.size >= 2 then
-      puts 'Za dużo argumentów.'
+      puts 'Too many arguments.'
     else
-      puts 'Nieznany błąd.'
+      puts 'Unknown bug.'
   end
 end
 
