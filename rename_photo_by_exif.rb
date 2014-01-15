0f87c1936da1982d5b0959ce8f82d276a7832fde
epik <epik@server.fake>
2014-01-15 14:25:16 +0100

    Working with photos taken in same second. TODO. Look to sorting photos in directory

--- rename_photo_by_exif.rb
+++ rename_photo_by_exif.rb
@@ -6,12 +6,30 @@ class RenamePhotoByExif
     when ARGV.size == 0 then
       puts 'Please, write path to photos as argument.'
     when ARGV.size == 1 then
+      dates = Hash.new(0)
+      files = Hash.new()
       path = ARGV[0]
+
       Dir.glob("#{path}/*.jpg") do |photo|
         exif = Exif::Parser.new(photo)
         new_name = exif['DateTimeOriginal'].to_s.gsub(':', '_').sub(' ', '__')
-        File.rename(photo, path + '/' + new_name + '.jpg')
+        dates[new_name] += 1
+        files[photo] = new_name
       end
+
+      dates.each_key do |key|
+        dates[key] += 1 if dates[key] > 1
+      end
+
+      files.each_pair do |file, name|
+        if dates[name] > 1
+          File.rename(file, path + '/' + name + "_#{dates[name]-1}" + '.jpg')
+          dates[name] -= 1
+        else
+          File.rename(file, path + '/' + name + '.jpg')
+        end
+      end
+
     when ARGV.size >= 2 then
       puts 'Too many arguments.'
     else
